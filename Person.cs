1aab695378cc22d50823248e5b73eaf46d51ee42
Florencia <117230347+florolivera@users.noreply.github.com>
2022-11-28 10:45:11 -0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDataBinding
+{
+    class Person
+    {
+        private string nameValue;
+        private double ageValue;
+        public String Name
+        {
+            get { return nameValue; }
+            set { nameValue = value; }
+        }
+        public double Age
+        {
+            get { return ageValue; }
+            set
+            {
+                if (value != ageValue)
+                {
+                    ageValue = value;
+                }
+            }
+
+        }
+    }
+}
